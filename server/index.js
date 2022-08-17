a9eb042b2aaf1b7f58a9acc868febb9131beb58b
Mallmann <codemallmann@gmail.com>
2022-08-17 11:46:21 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const cors = require("cors");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "crudgames",
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+
+  let mysql = "SELECT COUNT(*) as NUMREG from games name WHERE name = ? AND cost = ? AND category = ?";
+  db.query(mysql, [name, cost, category], (err, result) => {
+    if (result[0].NUMREG > 0) {
+      console.log("JA TA CADASTRADO");
+      res.send(result)
+
+    } else {
+      mysql = "INSERT INTO games ( name, cost, category) VALUES (?, ?, ?)";
+      db.query(mysql, [name, cost, category], (err, result) => {
+        res.send(result);
+      });
+    }
+
+  });
+
+
+  
+});
+
+app.post("/search", (req, res) => {
+  // const { name } = req.body;
+  // const { cost } = req.body;
+  // const { category } = req.body;
+  const { id } = req.body;
+
+  console.log(id);
+
+  let mysql =
+    "SELECT * from games WHERE id = ?";
+  db.query(mysql, [id], (err, result) => {
+    if (err) res.send(err);
+    res.send(result);
+  });
+});
+
+app.get("/getCards", (req, res) => {
+  let mysql = "SELECT * FROM games";
+  db.query(mysql, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/edit", (req, res) => {
+  const { id } = req.body;
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
+  let mysql = "UPDATE games SET name = ?, cost = ?, category = ? WHERE id = ?";
+  db.query(mysql, [name, cost, category, id], (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  let mysql = "DELETE FROM games WHERE id = ?";
+  db.query(mysql, id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("rodando na porta 3001");
+});
